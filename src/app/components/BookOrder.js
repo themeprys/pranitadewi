227b44af0794872cbd7af385c18d740db101176c
themeprys <themeprys@gmail.com>
2025-03-31 18:49:00 +0800

    add jbs

--- src/app/components/BookOrder.js
+++ src/app/components/BookOrder.js
@@ -21,7 +21,7 @@ function BookOrder() {
         </h2>
         <p><strong>Judul Buku:</strong> Nyawa Tinggallah Sejenak Lebih Lama, Kumpulan Puisi<br /><strong>Penulis:</strong> Pranita Dewi<br /><strong>Penerbit:</strong> JBS (Yogyakarta)<br /><strong>Tahun:</strong> 2024<br /><strong>Jumlah Hal:</strong> 88 hal<br /><strong>Dimensi:</strong> 13 x 19 cm<br /><strong>No ISBN:</strong> 9786237904885</p>
 <p><strong>STOCK:</strong> TERSEDIA</p>
-<Link href="https://wa.me/+6285773761554?text=Hi,%20saya%20ingin%20memesan%20buku%20puisi%20Pranita%20Dewi%0AJudul%20Buku:%0AJumlah%20Pesanan:%0ANama%20Pemesan:%0ADikirim%20ke%20alamat:">
+<Link href="https://wa.me/+6281802717528?text=Hi,%20saya%20ingin%20memesan%20buku%20puisi%20Pranita%20Dewi%0AJudul%20Buku:%0AJumlah%20Pesanan:%0ANama%20Pemesan:%0ADikirim%20ke%20alamat:">
 <Image src="/orderwa.webp"
             width="378"
             height="80"
