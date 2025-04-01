3ce2b3fcda7686fbed9bc0c0d53cb5847774c83c
themeprys <themeprys@gmail.com>
2025-04-01 17:17:24 +0800

    add cv

--- src/app/components/Heropage.js
+++ src/app/components/Heropage.js
@@ -1,10 +1,5 @@
 import React from 'react'
 import Hero from './Hero'
-import Intro from './Intro'
-import Stack from './Stack'
-import Porto from './Porto'
-import Leads from './Leads'
-import Skills from './Skills'
 import BookOrder from './BookOrder'
 
 const Heropage = () => {
