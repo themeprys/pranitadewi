fed88900628d58b9040095ffcd3f661553298a6b
themeprys <themeprys@gmail.com>
2025-04-01 17:08:11 +0800

    feat: enhance header layout - adjust navigation width and spacing

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -11,7 +11,7 @@ function Header() {
   return (
     <header className="border-bottom">
       <div className="row align-items-center">
-        <div className="col-md-4">
+        <div className="col-md-3">
           <Link
             href="/"
             className="text-body-emphasis text-decoration-none"
@@ -24,12 +24,12 @@ function Header() {
               </span>
           </Link>
         </div>
-        <div className="col-md-4 d-flex justify-content-center">
+        <div className="col-md-6 d-flex justify-content-center">
           <nav className="navbar navbar-expand-lg navbar-light">
             <div className="container-fluid">
               <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
-                  <li className="nav-item">
+                  <li className="nav-item me-4">
                     <Link 
                       href="/" 
                       className={`nav-link fw-bold fs-5 ${styles.hoverBrown} ${pathname === '/' ? styles.active : ''}`} 
@@ -38,7 +38,7 @@ function Header() {
                       Beranda
                     </Link>
                   </li>
-                  <li className="nav-item">
+                  <li className="nav-item me-4">
                     <Link 
                       href="/about" 
                       className={`nav-link fw-bold fs-5 ${styles.hoverBrown} ${pathname === '/about' ? styles.active : ''}`} 
@@ -47,7 +47,7 @@ function Header() {
                       Tentang
                     </Link>
                   </li>
-                  <li className="nav-item">
+                  <li className="nav-item me-4">
                     <Link 
                       href="/blog" 
                       className={`nav-link fw-bold fs-5 ${styles.hoverBrown} ${pathname === '/blog' ? styles.active : ''}`} 
@@ -56,12 +56,22 @@ function Header() {
                       Blog
                     </Link>
                   </li>
+                  <li className="nav-item">
+                    <Link 
+                      href="https://cdn.cosmicjs.com/c9260e30-0ed6-11f0-91ec-af6adca2ead2-Curriculum-Vitae-Pranita-Dewi-Updated-April-2025.pdf" 
+                      className={`nav-link fw-bold fs-5 ${styles.hoverBrown}`} 
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Unduh CV
+                    </Link>
+                  </li>
                 </ul>
               </div>
             </div>
           </nav>
         </div>
-        <div className="col-md-4 text-md-end">
+        <div className="col-md-3 text-md-end">
           <div className="thm_headerlinks">
             {/* <p className="mb-0"><a target="_blank" href="https://linkedin.com/in/pranitadewi">https://linkedin.com/in/pranitadewi</a><br/> */}
             <p>Email: <a href="mailto:duniapranita@gmail.com">duniapranita@gmail.com</a></p>
